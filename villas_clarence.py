85b7887fc36df5c63da3fafbe02bfac92bfd02e0
villas-clarence <villasclarence56@gmail.com>
2024-10-06 18:39:20 +0800

    add member info

--- villas_clarence.py
+++ villas_clarence.py
@@ -1,6 +1,8 @@
 name = "Clarence  Villas"
 birthday = "\nOctober 4, 2004"
 gender = "\nFemale"
-email = "\nvillasclarennce56@gmail.com"
+email = "\nvillasclarence56@gmail.com"
 
 print (name, birthday, gender, email)
+dm ="Welcome to the Team!- Hannah"
+print (dm)
