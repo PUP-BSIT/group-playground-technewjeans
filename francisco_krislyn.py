a8361baad8dc3e1291003ed8a73796fc4a187ae4
Krislyn Janelle Francisco <krislynfrancisco0815@gmail.com>
2024-11-25 14:03:11 +0800

    upload francisco module

--- francisco_krislyn.py
+++
@@ -1,8 +0,0 @@
-Name = "Krislyn Janelle Francisco"
-Birthday = "\nAugust 20,2005"
-Gender = "\nFemale"
-Email = "\nkrislynfrancisco0815@gmail.com"
-
-print (Name, Birthday, Gender, Email)
-dm = "Welcome to my file!"
-print (dm)
--- main1.py
+++ main1.py
@@ -1,5 +1,6 @@
-from group_package import uy_module, villas_module, genandoy_module
+from group_package import uy_module, villas_module, genandoy_module, francisco_module
 
 uy_module.main()
 villas_module.greet_with_emoji()
 genandoy_module.main() 
+francisco_module.calculate_area_and_input()
