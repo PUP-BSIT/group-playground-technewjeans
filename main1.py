b64eabbdd95985c48b7fe762e68c93f7262ea3ce
Hannah Genandoy <lorainneh540@gmail.com>
2024-11-25 13:31:41 +0800

    upload new module

--- main1.py
+++ main1.py
@@ -1,4 +1,5 @@
-from group_package import uy_module, villas_module
+from group_package import uy_module, villas_module, genandoy_module
 
 uy_module.main()
 villas_module.greet_with_emoji()
+genandoy_module.main() 
