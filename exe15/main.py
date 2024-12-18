aee196cbc68ea0ff4b84c2e3b23951202a95b22e
uy-angelicajoy <angelicajoyuy16@gmail.com>
2024-12-18 15:07:41 +0800

    refactor main.py

--- exe15/main.py
+++ exe15/main.py
@@ -1,54 +1,62 @@
 import os
-from technewjeans_package.uy import Game
-from technewjeans_package.mejares import GamblingAddiction
-from technewjeans_package.genandoy import OrderSystem
-from technewjeans_package.villas import Music
 from technewjeans_package.francisco import Information
+from technewjeans_package.genandoy import OrderSystem
+from technewjeans_package.mejares import GamblingAddiction
+from technewjeans_package.uy import Game
+from technewjeans_package.villas import Music
+
+EXIT_OPTION = " "
+UNSET_OPTION = "6"
 
 def main():
-    order_system = OrderSystem() 
+    choice = UNSET_OPTION
+    while choice != EXIT_OPTION:
+        choice = display_choice()
+        process_choice(choice)
 
-    while True:
-        os.system('cls')
-        print("Main Menu:")
-        print("1. Krislyn Francisco")
-        print("2. Hannah Lorraine Genandoy")
-        print("3. James Michael Mejares")
-        print("4. Angelica Joy Uy")
-        print("5. Clarence Villas")
-        print("6. Exit")
-        choice = input("Enter your choice: ")
+def display_choice():
+    os.system('cls')
+    print("Main Menu:")
+    print("1. Krislyn Francisco")
+    print("2. Hannah Lorraine Genandoy")
+    print("3. James Michael Mejares")
+    print("4. Angelica Joy Uy")
+    print("5. Clarence Villas")
+    print("6. Exit")
 
-        match choice:
-            case "1":
-                os.system('cls')
-                information = Information(age=19, city="Taguig City", school="PUP-Taguig")
-                information.menu()
-            case "2":
-                os.system('cls')
-                order_system.customer = input("Enter customer name: ")
-                order_system.menu()
-            case "3":
-                os.system('cls')
-                gamble = GamblingAddiction(profile={}, balance=0.0, luck=0.0)
-                gamble.mejares_menu()
-            case "4":
-                os.system('cls')
-                Game.uy_menu()
-            case "5":
-                os.system('cls')
-                song_title = input("Enter the Song Title: ")
-                song_writer = input("Enter the Song Writer: ")
-                genre = input("Enter the Genre: ")
-                year_release = input("Enter the Year of Release: ")
-                music1 = Music(song_title, song_writer, genre, year_release)
+    return input("Enter your choice: ")
+
+def process_choice(choice):
+    order_system = OrderSystem()
     
-                music1.menu()
-            case "6":
-                print("Exiting the program. Goodbye!")
-                break
-            case _:
-                print("Invalid choice. Please try again.")
+    match choice:
+        case "1":
+            os.system('cls')
+            information = Information(age=19, city="Taguig City", school="PUP-Taguig")
+            information.menu()
+        case "2":
+            os.system('cls')
+            order_system.customer = input("Enter customer name: ")
+            order_system.menu()
+        case "3":
+            os.system('cls')
+            gamble = GamblingAddiction(profile={}, balance=0.0, luck=0.0)
+            gamble.mejares_menu()
+        case "4":
+            os.system('cls')
+            Game.game_menu()
+        case "5":
+            os.system('cls')
+            song_title = input("Enter the Song Title: ")
+            song_writer = input("Enter the Song Writer: ")
+            genre = input("Enter the Genre: ")
+            year_release = input("Enter the Year of Release: ")
+            music1 = Music(song_title, song_writer, genre, year_release)
+            music1.menu()
+        case "6":
+            pass
+        case _:
+            print("Invalid choice. Please try again.")
 
 if __name__ == "__main__":
     main()
